b159ecef3eda77e94252a71aa40329912c3922e2
Dmitriy <vkosmos26@yandex.ru>
2019-07-17 14:08:39 +0300

    Create blockchain with new genesis

--- blockchain.go
+++ blockchain.go
@@ -1,11 +1,32 @@
 package main
 
+import "fmt"
+
 type Blockchain struct {
 	blocks []*Block
 }
 
-func (bc Blockchain) AddBlock(data string) {
+func (bc *Blockchain) AddBlock(data string) {
 	prevBlock := bc.blocks[len(bc.blocks)-1]
 	newBlock := NewBlock(data, prevBlock.Hash)
 	bc.blocks = append(bc.blocks, newBlock)
+}
+
+func (bc *Blockchain) newGenesis() {
+	block := NewBlock("Genesis block", []byte{})
+	bc.blocks = append(bc.blocks, block)
+}
+
+func NewBlockchain() *Blockchain {
+	bc := &Blockchain{}
+	bc.newGenesis()
+	return bc
+}
+
+func (bc *Blockchain) PrintLastBlock() {
+	lastBlock := bc.blocks[len(bc.blocks)-1]
+	fmt.Printf("Hash: %x\n", lastBlock.Hash)
+	fmt.Println("Data:", string(lastBlock.Data))
+	fmt.Printf("PrevHash: %x\n", lastBlock.PrevHash)
+	fmt.Println()
 }
--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
 func main() {
+	bc := NewBlockchain()
+	bc.PrintLastBlock()
+
+	bc.AddBlock("Send 1 BTC to Petya")
+	bc.PrintLastBlock()
+
+	bc.AddBlock("Send 2 BTC to Alex")
+	bc.PrintLastBlock()
 
 }
